4f90d47c579e51fdffd671ad24bef33a1d6e7e4d
mairalima <maijucelima@gmail.com>
2025-02-17 22:23:48 -0300

    Atualização de telas e código projeto

--- app/src/main/java/com/devspace/taskbeats/TaskDao.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskDao.kt
@@ -2,8 +2,10 @@ package com.devspace.taskbeats
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Delete
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface TaskDao {
@@ -12,4 +14,15 @@ interface TaskDao {
 
     @Query("SELECT * FROM taskentity")
     fun getAll(): List<TaskEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(taskEntity: TaskEntity)
+
+    @Update
+    fun update(taskEntity: TaskEntity)
+
+    @Delete
+    fun delete (taskEntity: TaskEntity)
+
+
 }
